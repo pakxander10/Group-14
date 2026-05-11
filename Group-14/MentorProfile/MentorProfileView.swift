9ec15ad54b68cb31700b9905688ecb08da46fd04
Medha Kuchimanchi <mkuchi@unc.edu>
2026-05-11 15:14:04 -0400

    Base code

---
+++ Group-14/MentorProfile/MentorProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MentorProfileView.swift
+//  Group-14
+//
+//  Created by Medha Kuchimanchi on 5/11/26.
+//
+
+import SwiftUI
+
+struct MentorProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MentorProfileView()
+}
