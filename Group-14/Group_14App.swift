29a7d2e2c9f2d89fc7d716fa5d8125cc945b42e9
Medha Kuchimanchi <mkuchi@unc.edu>
2026-05-11 15:04:25 -0400

    Initial Commit

---
+++ Group-14/Group_14App.swift
@@ -0,0 +1,17 @@
+//
+//  Group_14App.swift
+//  Group-14
+//
+//  Created by Medha Kuchimanchi on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Group_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
